e8d175e984703cc9250d3ba1a2154522bb2274f7
Benjamin Bolte <ben@bolte.cc>
2024-02-06 13:52:25 -0800

    more cleanup

--- mlfab/task/mixins/data_loader.py
+++ mlfab/task/mixins/data_loader.py
@@ -22,8 +22,6 @@ Batch = TypeVar("Batch")
 
 @dataclass
 class DataloaderConfig:
-    batch_size: int = field(MISSING, help="Size of each batch")
-    batch_size_multiplier: float = field(MISSING, help="Batch size multiplier")
     num_workers: int = field(MISSING, help="Number of workers for loading samples")
     prefetch_factor: int = field(2, help="Number of items to pre-fetch on each worker")
 
@@ -31,17 +29,12 @@ class DataloaderConfig:
 @dataclass
 class DataloadersConfig(ProcessConfig, BaseConfig):
     batch_size: int = field(MISSING, help="Size of each batch")
-    num_dataloader_workers: int = field(II("mlfab.num_workers:-1"), help="Default number of dataloader workers")
     train_dl: DataloaderConfig = field(
-        DataloaderConfig(
-            num_workers=II("num_dataloader_workers"),
-        ),
+        DataloaderConfig(num_workers=II("mlfab.num_workers:-1")),
         help="Train dataloader config",
     )
     test_dl: DataloaderConfig = field(
-        DataloaderConfig(
-            num_workers=1,
-        ),
+        DataloaderConfig(num_workers=1),
         help="Valid dataloader config",
     )
     debug_dataloader: bool = field(False, help="Debug dataloaders")
@@ -52,9 +45,7 @@ Config = TypeVar("Config", bound=DataloadersConfig)
 
 class DataloadersMixin(ProcessMixin[Config], BaseTask[Config], Generic[Config]):
     def __init__(self, config: Config) -> None:
-        if is_missing(config, "batch_size") and (
-            is_missing(config.train_dl, "batch_size") or is_missing(config.test_dl, "batch_size")
-        ):
+        if is_missing(config, "batch_size"):
             config.batch_size = self.get_batch_size()
 
         super().__init__(config)
@@ -97,7 +88,7 @@ class DataloadersMixin(ProcessMixin[Config], BaseTask[Config], Generic[Config]):
         return Dataloader(
             dataset=dataset,
             num_workers=0 if debugging else cfg.num_workers,
-            batch_size=round(cfg.batch_size * cfg.batch_size_multiplier),
+            batch_size=self.config.batch_size,
             prefetch_factor=cfg.prefetch_factor,
             ctx=self.multiprocessing_context,
             dataloader_worker_init_fn=self.dataloader_worker_init_fn,
