1ec9bff6e0b57c3e20d8061d0004c038569fce8f
Benjamin Bolte <ben@bolte.cc>
2024-05-07 17:51:19 -0700

    correct logging

--- mlfab/nn/device/auto.py
+++ mlfab/nn/device/auto.py
@@ -6,7 +6,6 @@ from mlfab.nn.device.base import base_device
 from mlfab.nn.device.cpu import cpu_device
 from mlfab.nn.device.gpu import gpu_device
 from mlfab.nn.device.metal import metal_device
-from mlfab.utils.logging import LOG_PING
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -21,7 +20,5 @@ ALL_DEVICE_TYPES: list[type[base_device]] = [
 def detect_device() -> base_device:
     for device_type in ALL_DEVICE_TYPES:
         if device_type.has_device():
-            device = device_type()
-            logger.log(LOG_PING, f"Using device: {device}")
-            return device
+            return device_type()
     raise RuntimeError("Could not automatically detect the device to use")
