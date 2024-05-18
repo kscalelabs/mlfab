74462b226dc5a666b8bdaa951d012b93ed6bc0c5
Benjamin Bolte <ben@bolte.cc>
2024-05-17 19:53:48 -0700

    fix failing tests

--- tests/nn/test_norms.py
+++ tests/nn/test_norms.py
@@ -1,14 +1,24 @@
 """Tests the norms API."""
 
-from typing import get_args
-
 import pytest
 import torch
 
 import mlfab
 
+conv_norm_types: list[mlfab.NormType] = [
+    "no_norm",
+    "batch",
+    "batch_affine",
+    "instance",
+    "instance_affine",
+    "group",
+    "group_affine",
+    "layer",
+    "layer_affine",
+]
 
-@pytest.mark.parametrize("norm_type", get_args(mlfab.NormType))
+
+@pytest.mark.parametrize("norm_type", conv_norm_types)
 def test_norm_1d(norm_type: mlfab.NormType) -> None:
     norm = mlfab.get_norm_1d(norm_type, dim=8, groups=2)
     x = torch.randn(3, 8, 5)
@@ -16,7 +26,7 @@ def test_norm_1d(norm_type: mlfab.NormType) -> None:
     assert y.shape == (3, 8, 5)
 
 
-@pytest.mark.parametrize("norm_type", get_args(mlfab.NormType))
+@pytest.mark.parametrize("norm_type", conv_norm_types)
 def test_norm_2d(norm_type: mlfab.NormType) -> None:
     norm = mlfab.get_norm_2d(norm_type, dim=8, groups=2)
     x = torch.randn(3, 8, 5, 3)
@@ -24,7 +34,7 @@ def test_norm_2d(norm_type: mlfab.NormType) -> None:
     assert y.shape == (3, 8, 5, 3)
 
 
-@pytest.mark.parametrize("norm_type", get_args(mlfab.NormType))
+@pytest.mark.parametrize("norm_type", conv_norm_types)
 def test_norm_3d(norm_type: mlfab.NormType) -> None:
     norm = mlfab.get_norm_3d(norm_type, dim=8, groups=2)
     x = torch.randn(3, 8, 5, 3, 2)
@@ -32,7 +42,13 @@ def test_norm_3d(norm_type: mlfab.NormType) -> None:
     assert y.shape == (3, 8, 5, 3, 2)
 
 
-linear_norm_types: list[mlfab.NormType] = ["no_norm", "batch", "batch_affine", "layer", "layer_affine"]
+linear_norm_types: list[mlfab.NormType] = [
+    "no_norm",
+    "batch",
+    "batch_affine",
+    "layer",
+    "layer_affine",
+]
 
 
 @pytest.mark.parametrize("norm_type", linear_norm_types)
