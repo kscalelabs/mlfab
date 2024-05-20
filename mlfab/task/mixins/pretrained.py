4093ff07a835b4b556f9196ee6e8dd44ce9e8b3c
Benjamin Bolte <ben@bolte.cc>
2024-05-19 17:06:48 -0700

    more fixes

--- mlfab/task/mixins/pretrained.py
+++ mlfab/task/mixins/pretrained.py
@@ -14,7 +14,7 @@ model parameters in the task state checkpoint or doing train-eval mode changes.
 
 from abc import ABC
 from dataclasses import dataclass
-from typing import Any, Callable, Generic, Self, TypeVar
+from typing import Any, Callable, Generic, Self, TypeVar, cast
 
 import torch
 from torch import Tensor, nn
@@ -30,6 +30,8 @@ class PretrainedConfig(BaseConfig):
 
 Config = TypeVar("Config", bound=PretrainedConfig)
 
+Tmod = TypeVar("Tmod", bound=nn.Module)
+
 
 class PretrainedModule:
     def __init__(self, module: nn.Module) -> None:
@@ -40,13 +42,9 @@ class PretrainedModule:
         self.module.requires_grad_(False)
 
     def __getattribute__(self, name: str) -> Any:
-        if name in ["module", "_apply", "forward", "__call__"]:
+        if name in ["module", "forward", "__call__"]:
             return super().__getattribute__(name)
         return getattr(self.module, name)
-
-    def _apply(self, fn: Callable[[Tensor], Tensor], recurse: bool = True) -> Self:
-        self.module._apply(fn, recurse)
-        return self
 
     @torch.no_grad()
     def forward(self, *args, **kwargs) -> Any:  # noqa: ANN401, ANN002, ANN003
@@ -57,8 +55,8 @@ class PretrainedModule:
         return self.module.__call__(*args, **kwargs)
 
 
-def pretrained(module: nn.Module) -> PretrainedModule:
-    return PretrainedModule(module)
+def pretrained(module: Tmod) -> Tmod:
+    return cast(Tmod, PretrainedModule(module))
 
 
 class PretrainedMixin(BaseTask[Config], Generic[Config], ABC):
@@ -76,5 +74,5 @@ class PretrainedMixin(BaseTask[Config], Generic[Config], ABC):
     def _apply(self, fn: Callable[[Tensor], Tensor], recurse: bool = True) -> Self:
         mod = super()._apply(fn, recurse)
         for pretrained_i in mod._pretrained_modules:
-            pretrained_i._apply(fn, recurse)
+            pretrained_i.module._apply(fn, recurse)
         return mod
