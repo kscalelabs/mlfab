45ff8abd76e8be7103900d8df6f8470a7888548d
Benjamin Bolte <ben@bolte.cc>
2024-06-27 04:58:21 +0800

    comment change

--- examples/parallel_mnist.py
+++ examples/parallel_mnist.py
@@ -21,7 +21,7 @@ class Config(mlfab.Config):
     weight_decay: float = mlfab.field(1e-4, help="Weight decay to use for the optimizer")
     warmup_steps: int = mlfab.field(100, help="Number of warmup steps to use for the optimizer")
 
-    # Model parallelism.
+    # Row and column parallel linear layers will have this many splits.
     model_parallelism: int = mlfab.field(2, help="Model parallelism")
 
 
