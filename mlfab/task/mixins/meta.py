e420079d6a482040fcfcc6a627492b0bc3430371
Benjamin Bolte <ben@bolte.cc>
2024-07-08 10:00:35 -0400

    back to original

--- mlfab/task/mixins/meta.py
+++ mlfab/task/mixins/meta.py
@@ -44,7 +44,14 @@ class MetaMixin(DeviceMixin[Config], Generic[Config]):
         self.reset_parameters_(model)
 
     def to_empty_if_meta(self, t: Tensor) -> Tensor:
-        return torch.empty_like(t, device=self.torch_device) if t.is_meta else t.to(self.torch_device)
+        if t.is_meta:
+            if t.is_floating_point():
+                return torch.empty_like(t, device=self.torch_device, dtype=self.torch_dtype)
+            return torch.empty_like(t, device=self.torch_device)
+
+        if t.is_floating_point():
+            return t.to(self.torch_device, self.torch_dtype)
+        return t.to(self.torch_device)
 
     def reset_parameters_(self, model: nn.Module) -> None:
         """Recursively resets model parameters.
