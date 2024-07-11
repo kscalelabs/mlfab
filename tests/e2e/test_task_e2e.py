4298abe1008dd7cd791a3c5b6ddb6af4e256e24b
Benjamin Bolte <ben@bolte.cc>
2024-07-10 17:26:26 -0700

    stuff

--- tests/e2e/test_task_e2e.py
+++ tests/e2e/test_task_e2e.py
@@ -83,10 +83,9 @@ def test_e2e_training_mp(tmpdir: Path, model_parallelism: int) -> None:
 
     DummyTask.launch(config, launcher=mlfab.MultiProcessLauncher(num_processes=num_processes), use_cli=False)
 
+    # Run from the same experiment directory.
     exp_dir = tmpdir / "dummy_task" / "run_0"
     assert exp_dir.exists(), f"Experiment directory {tmpdir} contains files {list(tmpdir.iterdir())}"
-
-    # Run from the same experiment directory.
     config.exp_dir = str(exp_dir)
     config.max_steps = 10
 
